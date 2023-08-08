b376c59e30d258bae020b0ddeb8b08dbeb984ed1
Godson Joseph <109551608+0ut1s509@users.noreply.github.com>
2023-08-07 20:49:02 -0400

    Create enonse_2.sql
    2

---
+++ enonse_2.sql
@@ -0,0 +1,22 @@
+
+CREATE DATABASE Boutik;
+
+USE Boutik;
+
+
+CREATE TABLE Pwodwi (
+    non VARCHAR(255) NOT NULL,
+    pri FLOAT NOT NULL,
+);
+
+
+CREATE TABLE Kategori (
+    non VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE Kliyan (
+    non VARCHAR(255) NOT NULL,
+    adrès VARCHAR(255),
+    telefòn VARCHAR(20)
+);
